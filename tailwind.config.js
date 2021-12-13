219f617d272410c8845b5608219701180ae86714
AlexisVS <63750707+AlexisVS@users.noreply.github.com>
2021-12-13 12:44:50 +0100

    ok

--- tailwind.config.js
+++ tailwind.config.js
@@ -3,11 +3,14 @@ module.exports = {
   
   content: ['./index.html', './src/**/*.{vue,js,ts,jsx,tsx}'],
   safelist: [
-    {
-      pattern: /bg-(slate|gray|neutral|stone|red|orange|amber|yellow|lime|green|emerald|teal|cyan|sky|blue|indigo|violet|purple|fuchsia|pink|rose)-(50|100|200|300|400|500|600|700|800|900)/,
-      variants: ['hover', 'focus'],
-
-    },
+        {
+          pattern :/bg-(slate|gray|neutral|stone|red|orange|amber|yellow|lime|green|emerald|teal|cyan|sky|blue|indigo|violet|purple|fuchsia|pink|rose)-(50|100|200|300|400|500|600|700|800|900)/,
+          variants: ['hover', 'focus'],
+        },
+        {
+          pattern: /text-(slate|gray|neutral|stone|red|orange|amber|yellow|lime|green|emerald|teal|cyan|sky|blue|indigo|violet|purple|fuchsia|pink|rose)-(50|100|200|300|400|500|600|700|800|900)/,
+          variants: ['hover', 'focus'],
+        },
   ],
   presets: [],
   darkMode: 'media', // or 'class'
